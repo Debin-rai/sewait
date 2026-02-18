c338a968237169f349158bbcd98330cc8413f3d4
Debin-rai <rai.debin@gmail.com>
2026-02-18 14:57:23 +0545

    fix: remove lenis dependency from SmoothScroll to fix Railway build

--- src/components/SmoothScroll.tsx
+++ src/components/SmoothScroll.tsx
@@ -1,34 +1,12 @@
 "use client";
 
-import { ReactNode, useEffect } from "react";
-import Lenis from "lenis";
+import { ReactNode } from "react";
 
 interface SmoothScrollProps {
     children: ReactNode;
 }
 
+// Smooth scrolling is handled via CSS scroll-behavior in globals.css
 export default function SmoothScroll({ children }: SmoothScrollProps) {
-    useEffect(() => {
-        const lenis = new Lenis({
-            duration: 1.2,
-            easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-            orientation: "vertical",
-            gestureOrientation: "vertical",
-            smoothWheel: true,
-            touchMultiplier: 2,
-        });
-
-        function raf(time: number) {
-            lenis.raf(time);
-            requestAnimationFrame(raf);
-        }
-
-        requestAnimationFrame(raf);
-
-        return () => {
-            lenis.destroy();
-        };
-    }, []);
-
     return <>{children}</>;
 }
