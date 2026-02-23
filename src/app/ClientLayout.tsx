a9bd76a5de642de11cceedcc36ad8f23a5573db4
Debin-rai <rai.debin@gmail.com>
2026-02-23 20:54:02 +0545

    fix: remove orphaned imports in ClientLayout.tsx

--- src/app/ClientLayout.tsx
+++ src/app/ClientLayout.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 import { LanguageProvider } from "@/context/LanguageContext";
-import { ThemeProvider } from "@/context/ThemeContext";
-import ThemePickerPopup from "@/components/ThemePickerPopup";
 
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
     return (
